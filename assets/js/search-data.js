b48edec67266ccd4eda8680356212af8ad59ce1c
yul1024 <yul1024@outlook.com>
2025-12-02 04:34:39 +0000

    Deploying to gh-pages from @ yuliu625/yuliu625.github.io@4d55debae54d9c1ac109ec998b5c2e55f18497f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "Two accounts for academic and daily exploration work. Featuring a selection of useful projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
